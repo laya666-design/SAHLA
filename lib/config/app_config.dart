0a3fbdf135937c8303e9078e41dfabf92eaa8d18
laya666-design <laya666@gmail.com>
2026-08-25 20:17:10 +0100

    Add files via upload

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -6,17 +6,22 @@ class AppConfig {
   final String appName;
   final Color primaryColor;
   final Color primaryDark;
+  final String baridimobPhone;
 
   const AppConfig._({
     required this.appName,
     required this.primaryColor,
     required this.primaryDark,
+    required this.baridimobPhone,
   });
 
   static const _instance = AppConfig._(
     appName: 'El Bouni Pièces Auto',
     primaryColor: Color(0xFFDC2626), // rouge
     primaryDark: Color(0xFFA31515),
+    // Numéro BaridiMob où les magasins envoient leur paiement manuel
+    // d'abonnement (repli quand ils n'ont pas de carte CIB/Edahabia).
+    baridimobPhone: '0556653220',
   );
 
   static AppConfig current() => _instance;
